38dcb613144486dfa17fd512b4d422464f91db4a
Pavel Agafonov <pavel.agafonov.al@gmail.com>
2025-05-06 00:06:12 +0300

    rewrite: io with traits

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ use std::sync::mpsc::channel;
 use std::sync::{Arc, Mutex, Weak};
 use std::{fmt::Debug, net::TcpStream};
 
-use zerocopy::{FromBytes, Immutable, IntoBytes};
-
 pub trait GameObject: Sync + Send + Debug {}
 
 #[derive(Debug, Default)]
@@ -86,48 +84,6 @@ impl Default for Home {
 }
 
 impl GameObject for Home {}
-
-impl RoboCat {
-    pub fn write(&self, stream: &mut OutputMemoryStream, ctx: &mut LinkingContext) {
-        stream.write(&self.health);
-        stream.write(&self.meow_count);
-        stream.write(&self.mice_indices.len());
-        stream.write_many(self.mice_indices.as_slice());
-        stream.write(&self.name.len());
-        stream.write_many(&self.name.as_bytes());
-
-        if let Some(home) = self.home.as_ref().and_then(|go| go.upgrade()) {
-            stream.write(&1u8);
-            stream.write(&ctx.get_network_id(&home, false).unwrap());
-        } else {
-            stream.write(&0u8);
-        }
-    }
-
-    pub fn read(&mut self, stream: &mut InputMemoryStream, ctx: &LinkingContext) {
-        let mut count = 0usize;
-        stream.read(&mut self.health);
-        stream.read(&mut self.meow_count);
-        stream.read(&mut count);
-        self.mice_indices.resize(count, 0);
-        stream.read_many(self.mice_indices.as_mut_slice());
-        stream.read(&mut count);
-        let mut vec = vec![0u8; count];
-        stream.read_many(&mut vec);
-        self.name = String::from_utf8(vec).unwrap();
-
-        let mut flag = 0u8;
-        stream.read(&mut flag);
-
-        if flag != 0 {
-            let mut id = 0usize;
-            stream.read(&mut id);
-
-            self.home = Some(Arc::downgrade(&ctx.get_game_object(id).unwrap()));
-        }
-    }
-}
-
 impl GameObject for RoboCat {}
 
 impl Default for RoboCat {
@@ -154,20 +110,6 @@ impl OutputMemoryStream {
     }
 }
 
-impl OutputMemoryStream {
-    pub fn write<T: IntoBytes + Immutable>(&mut self, data: &T) {
-        self.buffer.extend(data.as_bytes());
-    }
-
-    pub fn write_many<T: IntoBytes + Immutable>(&mut self, data: &[T]) {
-        self.buffer.extend(data.as_bytes());
-    }
-
-    pub fn buffer(&self) -> &[u8] {
-        &self.buffer
-    }
-}
-
 pub struct InputMemoryStream {
     buffer: Vec<u8>,
     head: usize,
@@ -176,30 +118,333 @@ pub struct InputMemoryStream {
 impl InputMemoryStream {
     pub fn owned() -> Self {
         Self {
-            buffer: vec![0u8; 128],
+            buffer: vec![0u8; 1470],
             head: 0,
-        }
-    }
-
-    pub fn read<T: FromBytes>(&mut self, data: &mut T) {
-        let size = size_of::<T>();
-        let value = T::read_from_bytes(&self.buffer[self.head..(self.head + size)]).unwrap();
-        self.head += size;
-        *data = value;
-    }
-
-    pub fn read_many<T: FromBytes>(&mut self, data: &mut [T]) {
-        let size = size_of::<T>();
-
-        for v in data {
-            let value = T::read_from_bytes(&self.buffer[self.head..(self.head + size)]).unwrap();
-            self.head += size;
-            *v = value;
         }
     }
 
     pub fn buffer_mut(&mut self) -> &mut [u8] {
         self.buffer.as_mut()
+    }
+}
+
+pub trait WriteStream {
+    type Error;
+
+    fn buffer(&self) -> &[u8];
+
+    fn write_u8(&mut self, v: u8) -> Result<(), Self::Error>;
+    fn write_i8(&mut self, v: i8) -> Result<(), Self::Error>;
+
+    fn write_u32(&mut self, v: u32) -> Result<(), Self::Error>;
+    fn write_i32(&mut self, v: i32) -> Result<(), Self::Error>;
+
+    fn write_usize(&mut self, v: usize) -> Result<(), Self::Error>;
+}
+
+pub trait ReadStream {
+    type Error;
+
+    fn buffer(&self) -> &[u8];
+
+    fn read_u8(&mut self) -> Result<u8, Self::Error>;
+    fn read_i8(&mut self) -> Result<i8, Self::Error>;
+
+    fn read_u32(&mut self) -> Result<u32, Self::Error>;
+    fn read_i32(&mut self) -> Result<i32, Self::Error>;
+
+    fn read_usize(&mut self) -> Result<usize, Self::Error>;
+}
+
+impl WriteStream for OutputMemoryStream {
+    type Error = std::io::Error;
+
+    fn buffer(&self) -> &[u8] {
+        &self.buffer
+    }
+
+    fn write_u8(&mut self, v: u8) -> Result<(), Self::Error> {
+        self.buffer.extend(&v.to_le_bytes());
+
+        Ok(())
+    }
+
+    fn write_i8(&mut self, v: i8) -> Result<(), Self::Error> {
+        self.buffer.extend(&v.to_le_bytes());
+
+        Ok(())
+    }
+
+    fn write_u32(&mut self, v: u32) -> Result<(), Self::Error> {
+        self.buffer.extend(&v.to_le_bytes());
+
+        Ok(())
+    }
+
+    fn write_i32(&mut self, v: i32) -> Result<(), Self::Error> {
+        self.buffer.extend(&v.to_le_bytes());
+
+        Ok(())
+    }
+
+    fn write_usize(&mut self, v: usize) -> Result<(), Self::Error> {
+        self.buffer.extend(&v.to_le_bytes());
+
+        Ok(())
+    }
+}
+
+impl ReadStream for InputMemoryStream {
+    type Error = std::io::Error;
+
+    fn buffer(&self) -> &[u8] {
+        &self.buffer
+    }
+
+    fn read_u8(&mut self) -> Result<u8, Self::Error> {
+        let size = size_of::<u8>();
+        let v = u8::from_le_bytes(
+            self.buffer[self.head..(self.head + size)]
+                .try_into()
+                .unwrap(),
+        );
+        self.head += size;
+
+        Ok(v)
+    }
+
+    fn read_i8(&mut self) -> Result<i8, Self::Error> {
+        let size = size_of::<i8>();
+        let v = i8::from_le_bytes(
+            self.buffer[self.head..(self.head + size)]
+                .try_into()
+                .unwrap(),
+        );
+        self.head += size;
+
+        Ok(v)
+    }
+
+    fn read_u32(&mut self) -> Result<u32, Self::Error> {
+        let size = size_of::<u32>();
+        let v = u32::from_le_bytes(
+            self.buffer[self.head..(self.head + size)]
+                .try_into()
+                .unwrap(),
+        );
+        self.head += size;
+
+        Ok(v)
+    }
+
+    fn read_i32(&mut self) -> Result<i32, Self::Error> {
+        let size = size_of::<i32>();
+        let v = i32::from_le_bytes(
+            self.buffer[self.head..(self.head + size)]
+                .try_into()
+                .unwrap(),
+        );
+        self.head += size;
+
+        Ok(v)
+    }
+
+    fn read_usize(&mut self) -> Result<usize, Self::Error> {
+        let size = size_of::<usize>();
+        let v = usize::from_le_bytes(
+            self.buffer[self.head..(self.head + size)]
+                .try_into()
+                .unwrap(),
+        );
+        self.head += size;
+
+        Ok(v)
+    }
+}
+
+pub trait Readable: Sized {
+    type Ctx;
+
+    fn read<R: ReadStream>(stream: &mut R, ctx: &mut Self::Ctx) -> Result<Self, R::Error>;
+}
+
+pub trait Writable {
+    type Ctx;
+
+    fn write<W: WriteStream>(&self, stream: &mut W, ctx: &mut Self::Ctx) -> Result<(), W::Error>;
+}
+
+impl Writable for u8 {
+    type Ctx = LinkingContext;
+
+    fn write<W: WriteStream>(&self, stream: &mut W, _: &mut Self::Ctx) -> Result<(), W::Error> {
+        stream.write_u8(*self)?;
+
+        Ok(())
+    }
+}
+
+impl Readable for u8 {
+    type Ctx = LinkingContext;
+
+    fn read<R: ReadStream>(stream: &mut R, _: &mut Self::Ctx) -> Result<Self, R::Error> {
+        stream.read_u8()
+    }
+}
+
+impl Writable for u32 {
+    type Ctx = LinkingContext;
+
+    fn write<W: WriteStream>(&self, stream: &mut W, _: &mut Self::Ctx) -> Result<(), W::Error> {
+        stream.write_u32(*self)?;
+
+        Ok(())
+    }
+}
+
+impl Readable for u32 {
+    type Ctx = LinkingContext;
+
+    fn read<R: ReadStream>(stream: &mut R, _: &mut Self::Ctx) -> Result<Self, R::Error> {
+        stream.read_u32()
+    }
+}
+
+impl<T: Writable> Writable for &[T] {
+    type Ctx = T::Ctx;
+
+    fn write<W: WriteStream>(&self, stream: &mut W, ctx: &mut Self::Ctx) -> Result<(), W::Error> {
+        stream.write_usize(self.len())?;
+
+        for el in self.iter() {
+            el.write(stream, ctx)?;
+        }
+
+        Ok(())
+    }
+}
+
+impl<T: Writable> Writable for Vec<T> {
+    type Ctx = T::Ctx;
+
+    fn write<W: WriteStream>(&self, stream: &mut W, ctx: &mut Self::Ctx) -> Result<(), W::Error> {
+        self.as_slice().write(stream, ctx)
+    }
+}
+
+impl<T: Readable> Readable for Vec<T> {
+    type Ctx = T::Ctx;
+
+    fn read<R: ReadStream>(stream: &mut R, ctx: &mut Self::Ctx) -> Result<Self, R::Error> {
+        let count = stream.read_usize()?;
+        let mut vec = Vec::with_capacity(count);
+
+        for _ in 0..count {
+            vec.push(T::read(stream, ctx)?);
+        }
+
+        Ok(vec)
+    }
+}
+
+impl Writable for String {
+    type Ctx = LinkingContext;
+
+    fn write<W: WriteStream>(&self, stream: &mut W, ctx: &mut Self::Ctx) -> Result<(), W::Error> {
+        self.as_bytes().write(stream, ctx)
+    }
+}
+
+impl Readable for String {
+    type Ctx = LinkingContext;
+
+    fn read<R: ReadStream>(stream: &mut R, ctx: &mut Self::Ctx) -> Result<Self, R::Error> {
+        let vec = Vec::<u8>::read(stream, ctx)?;
+
+        Ok(String::from_utf8(vec).unwrap())
+    }
+}
+
+impl Writable for Option<Weak<dyn GameObject>> {
+    type Ctx = LinkingContext;
+
+    fn write<W: WriteStream>(&self, stream: &mut W, ctx: &mut Self::Ctx) -> Result<(), W::Error> {
+        self.as_ref().and_then(|x| x.upgrade()).write(stream, ctx)
+    }
+}
+
+impl Writable for Arc<dyn GameObject> {
+    type Ctx = LinkingContext;
+
+    fn write<W: WriteStream>(&self, stream: &mut W, ctx: &mut Self::Ctx) -> Result<(), W::Error> {
+        stream.write_usize(ctx.get_network_id(self, false).unwrap())?;
+
+        Ok(())
+    }
+}
+
+impl Readable for Arc<dyn GameObject> {
+    type Ctx = LinkingContext;
+
+    fn read<R: ReadStream>(stream: &mut R, ctx: &mut Self::Ctx) -> Result<Self, R::Error> {
+        let go = ctx.get_game_object(stream.read_usize()?).unwrap();
+
+        Ok(go)
+    }
+}
+
+impl Readable for Option<Weak<dyn GameObject>> {
+    type Ctx = LinkingContext;
+
+    fn read<R: ReadStream>(stream: &mut R, ctx: &mut Self::Ctx) -> Result<Self, R::Error> {
+        if stream.read_u8()? == 0 {
+            return Ok(None);
+        }
+
+        let go = ctx.get_game_object(stream.read_usize()?).unwrap();
+        Ok(Some(Arc::downgrade(&go)))
+    }
+}
+
+impl<T: Writable> Writable for Option<T> {
+    type Ctx = T::Ctx;
+
+    fn write<W: WriteStream>(&self, stream: &mut W, ctx: &mut Self::Ctx) -> Result<(), W::Error> {
+        match self {
+            Some(v) => {
+                stream.write_u8(1)?;
+                v.write(stream, ctx)
+            }
+            None => stream.write_u8(0),
+        }
+    }
+}
+
+impl Writable for RoboCat {
+    type Ctx = LinkingContext;
+
+    fn write<W: WriteStream>(&self, stream: &mut W, ctx: &mut Self::Ctx) -> Result<(), W::Error> {
+        self.health.write(stream, ctx)?;
+        self.meow_count.write(stream, ctx)?;
+        self.mice_indices.write(stream, ctx)?;
+        self.name.write(stream, ctx)?;
+        self.home.write(stream, ctx)?;
+
+        Ok(())
+    }
+}
+
+impl Readable for RoboCat {
+    type Ctx = LinkingContext;
+
+    fn read<R: ReadStream>(stream: &mut R, ctx: &mut Self::Ctx) -> Result<Self, R::Error> {
+        Ok(Self {
+            health: stream.read_u32()?,
+            meow_count: stream.read_u32()?,
+            mice_indices: Vec::<u32>::read(stream, ctx)?,
+            name: String::read(stream, ctx)?,
+            home: Option::<Weak<dyn GameObject>>::read(stream, ctx)?,
+        })
     }
 }
 
@@ -229,7 +474,7 @@ fn main() {
 
         rcv.recv().unwrap();
 
-        cat.write(&mut output, &mut *ctx1.lock().unwrap());
+        cat.write(&mut output, &mut *ctx1.lock().unwrap()).unwrap();
         stream.write(&output.buffer).unwrap();
         stream.flush().unwrap();
     });
@@ -242,8 +487,7 @@ fn main() {
 
     let mut input = InputMemoryStream::owned();
     client.read(input.buffer_mut()).unwrap();
-    let mut cat = RoboCat::default();
-    cat.read(&mut input, &*ctx.lock().unwrap());
+    let cat = RoboCat::read(&mut input, &mut *ctx.lock().unwrap()).unwrap();
 
     dbg!(cat);
 }
